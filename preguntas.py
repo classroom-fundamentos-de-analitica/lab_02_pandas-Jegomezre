62b755e0d7a0507721713a15ca2124058536bef4
Jeronimo Gomez Restrepo <jegomezre@unal.edu.co>
2024-05-06 17:03:59 -0500

    1

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,7 @@ def pregunta_01():
     40
 
     """
-    return
+    return len(tbl0)
 
 
 def pregunta_02():
@@ -33,7 +33,7 @@ def pregunta_02():
     4
 
     """
-    return
+    return len(tbl0.columns)
 
 
 def pregunta_03():
@@ -50,7 +50,7 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    return
+    return tbl0['_c1'].value_counts().sort_index()
 
 
 def pregunta_04():
@@ -65,7 +65,7 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return
+    return tbl0[['_c1', '_c2']].groupby(['_c1']).mean().squeeze()
 
 
 def pregunta_05():
@@ -82,7 +82,7 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0[['_c1', '_c2']].groupby(['_c1']).max().squeeze()
 
 
 def pregunta_06():
@@ -94,7 +94,7 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    return
+    return sorted(map(lambda x: x.upper(), tbl1['_c4'].unique()))
 
 
 def pregunta_07():
@@ -110,7 +110,7 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0[['_c1', '_c2']].groupby(['_c1']).sum().squeeze()
 
 
 def pregunta_08():
@@ -128,7 +128,11 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    return
+
+    Suma=tbl0.sum(numeric_only=True, axis=1).tolist()
+    Sumas=tbl0.copy()
+    Sumas['suma']=Suma
+    return Sumas
 
 
 def pregunta_09():
@@ -146,7 +150,12 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    return
+
+    c3 = tbl0['_c3'].tolist()
+    years = list(map(lambda x: x.split('-')[0], c3))
+    nuevoDf = tbl0.copy()
+    nuevoDf['year'] = years
+    return nuevoDf
 
 
 def pregunta_10():
@@ -163,7 +172,15 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    return
+
+    Grupo=tbl0[['_c1', '_c2']].groupby(['_c1'])['_c2'].apply(list).tolist()
+    c2=[]
+    for i in Grupo:
+        X=''
+        for j in sorted(i):
+            X+=f'{j}:'
+        c2.append(X[:-1])
+    return pd.DataFrame({'_c2': c2}, index=pd.ptos(['A', 'B', 'C', 'D', 'E'], name='_c1'))
 
 
 def pregunta_11():
@@ -182,7 +199,12 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
-    return
+    tbl04 = tbl1.copy()
+    tbl04['_c4'] = tbl04.groupby(['_c0'])['_c4'].transform(lambda x : ''.join(x))
+    tbl04 = tbl04.drop_duplicates()
+    tbl04["_c4"] = tbl04["_c4"].apply(sorted)
+    tbl04["_c4"] = tbl04["_c4"].apply(lambda x : ','.join(x))
+    return tbl04.reset_index()[["_c0", "_c4"]]
 
 
 def pregunta_12():
@@ -200,7 +222,16 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
-    return
+
+    tbl04 = tbl2.copy()
+    tbl04["_c5b"]=tbl04["_c5b"].apply(str)
+    tbl04=tbl04.sort_values(by=['_c5a', '_c5b'])
+    tbl04['_c5'] = tbl04[['_c5a', '_c5b']].agg(':'.join, axis=1)
+    tbl04['_c5'] = tbl04.groupby(['_c0'])['_c5'].transform(lambda x : ','.join(x))
+    tbl04 = tbl04[["_c0","_c5"]].drop_duplicates()
+    tbl04 = tbl04.sort_values("_c0")
+
+    return tbl04.reset_index()[["_c0", "_c5"]]
 
 
 def pregunta_13():
@@ -217,4 +248,9 @@ def pregunta_13():
     E    275
     Name: _c5b, dtype: int64
     """
-    return
+
+    join = pd.merge(tbl0, tbl2, on='_c0', how='inner')
+
+    aws = join[['_c1', '_c5b']].groupby(['_c1']).sum()
+    pto = aws.squeeze()
+    return pto
